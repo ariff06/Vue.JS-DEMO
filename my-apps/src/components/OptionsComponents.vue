f913ce1c7542b5b2ceda608d9a753454b9945b70
ariff06 <ariffauzy74@gmail.com>
2026-03-26 03:58:12 +0700

    Fix text

--- my-apps/src/components/OptionsComponents.vue
+++ my-apps/src/components/OptionsComponents.vue
@@ -18,5 +18,5 @@
 
 <template>
     <h1>{{ firstName }}{{ middleName }}{{ lastName }}</h1>
-    <button @click="showName">Show name !</button>    
+    <button @click="showName">Show name!</button>    
 </template>
