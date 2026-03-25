da8efbe0e888f32541d2546c4d36be1380fdcfd5
ariff06 <ariffauzy74@gmail.com>
2026-03-26 04:16:25 +0700

    Fixed or just a little change

--- my-apps/src/components/MainButton.vue
+++ my-apps/src/components/MainButton.vue
@@ -3,13 +3,14 @@
     const isLoading = true
     const buttonAttr = {
         disabled: isLoading,
+        id: 1
     }
 
     const showName = () => console.log(fullName)
 </script>
 
 <template>
-    <button v-bind="buttonAttr" :id="`id-${1}`" @click="showName">
+    <button v-bind="buttonAttr" :id="`id-${buttonAttr.id}`" @click="showName">
         {{ isLoading ? 'Loading' : 'Submit'}}
     </button>
 </template>
